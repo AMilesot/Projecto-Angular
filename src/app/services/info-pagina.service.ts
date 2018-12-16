9aa25bdf025f88a7fb0ff281fce1e0a08b6d869d
Amilesot <a.milesot@gmail.com>
2018-12-16 15:53:26 -0700

    Leyendo informacion de base de datos Firebase y mostrandola en el HTML (Fin de seccion 5)

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
+import { InfoEquipo } from '../interfaces/equipo.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +9,18 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
 
   info: InfoPagina = {};
+  equipo: InfoEquipo =  {};
   todoBien = false;
 
   constructor(private http: HttpClient) {
 
     console.log('Servicio al tiro!!!');
+
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
 
     // Leer arvhivo JSON
     this.http.get('assets/data/data-pagina.json')
@@ -20,11 +28,20 @@ export class InfoPaginaService {
 
               this.todoBien = true;
               this.info = resp;
-              console.log(resp);
+              console.log(this.info);
 
               // console.log(resp['twitter']);  Haciendo referencia a una propiedad del objeto
               console.log(resp.email); // Usando una interface para poder usar sus propiedades en codigo
 
             });
+  }
+
+  private cargarEquipo() {
+
+    this.http.get('https://angular-html-1eee0.firebaseio.com/equipo.json')
+             .subscribe( (resp: InfoEquipo) => {
+               this.equipo = resp;
+               console.log(this.equipo);
+             });
   }
 }
